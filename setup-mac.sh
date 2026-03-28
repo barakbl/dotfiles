50ab807596f55c011b29adb78c3239e5dcd72609
Barak Bloch <Barak Bloch>
2026-03-28 21:34:39 +0300

    init

---
+++ setup-mac.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+DOTFILES_DIR="$HOME/dotfiles"
+BREW_APPS=(docker lsd btop git  fzf zsh starship stow)
+
+echo "=============="
+echo "Dotfiles Setup"
+echo "=============="
+echo
+
+############### install brew packages ######################
+function brew_inst() {
+	if ! command -v $1 &> /dev/null; then
+		echo "$1 is not installed. Installing..."
+		sudo brew install $1
+		echo "$1 installed successfully!"
+	else
+		echo "$1 installed already, skip"
+	fi
+	echo ""
+}
+
+for app in "${BREW_APPS[@]}"; do
+	brew_inst $app
+done
+
+#brew tap homebrew/cask-fonts
+#brew install --cask font-blex-mono-nerd-font
+############ End brew ##################################=
+
