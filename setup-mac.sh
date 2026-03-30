0a02df6df57e7f983c9822451836f4a7eb9c7c20
Barak Bloch <Barak Bloch>
2026-03-30 18:18:23 +0300

    added autoload for python venv

--- setup-mac.sh
+++ setup-mac.sh
@@ -3,7 +3,7 @@
 set -e
 
 DOTFILES_DIR="$HOME/dotfiles"
-BREW_APPS=(ansifilter fd nerdfetch docker zsh-autosuggestions zplug lsd lazygit tmux btop git  fzf zsh starship stow)
+BREW_APPS=(ansifilter fd nerdfetch docker zsh-autosuggestions zplug lsd lazygit tmux btop git fzf zsh starship stow)
 
 echo "=============="
 echo "Dotfiles Setup"
@@ -12,21 +12,20 @@ echo
 
 ############### install brew packages ######################
 function brew_inst() {
-	if ! command -v $1 &> /dev/null; then
-		echo "$1 is not installed. Installing..."
-		brew install $1
-		echo "$1 installed successfully!"
-	else
-		echo "$1 installed already, skip"
-	fi
-	echo ""
+  if ! command -v $1 &>/dev/null; then
+    echo "$1 is not installed. Installing..."
+    brew install $1
+    echo "$1 installed successfully!"
+  else
+    echo "$1 installed already, skip"
+  fi
+  echo ""
 }
 
 for app in "${BREW_APPS[@]}"; do
-	brew_inst $app
+  brew_inst $app
 done
 
 #brew tap homebrew/cask-fonts
 #brew install --cask font-blex-mono-nerd-font
 ############ End brew ##################################=
-
