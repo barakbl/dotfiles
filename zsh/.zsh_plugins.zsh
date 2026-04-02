6937770039fb7de23f11174558ab27449ac4bd02
Barak Bloch <Barak Bloch>
2026-04-02 20:27:04 +0300

    remove zsh_plugins.zsh from git, it will load automaticlly by antidot

--- zsh/.zsh_plugins.zsh
+++
@@ -1,8 +0,0 @@
-fpath+=( "$HOME/Library/Caches/antidote/github.com/jeffreytse/zsh-vi-mode" )
-source "$HOME/Library/Caches/antidote/github.com/jeffreytse/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
-if ! (( $+functions[zsh-defer] )); then
-  fpath+=( "$HOME/Library/Caches/antidote/github.com/romkatv/zsh-defer" )
-  source "$HOME/Library/Caches/antidote/github.com/romkatv/zsh-defer/zsh-defer.plugin.zsh"
-fi
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zdharma-continuum/fast-syntax-highlighting" )
-zsh-defer source "$HOME/Library/Caches/antidote/github.com/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
